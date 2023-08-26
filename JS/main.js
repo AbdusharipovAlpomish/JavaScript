806afa68310495aace43aa72f6f1a212574c0ce7
AbdusharipovAlpomish <138755294+AbdusharipovAlpomish@users.noreply.github.com>
2023-08-26 11:12:05 +0500

    boldi

---
+++ JS/main.js
@@ -0,0 +1,44 @@
+// 1 masala
+
+// let str = ["I", "study", "JavaScript", "right", "now"];
+
+// str = str.slice(2);
+
+// console.log(str);
+
+// 2 masala
+
+// let string1 = "salom dunyo";
+
+// function str1(params) {
+//   let counter = "";
+
+//   for (let i = 0; i < params.length; i++) {
+//     counter = counter + i;
+//   }
+//   console.log(counter);
+// }
+
+// str1(string1);
+
+// 3 masala
+
+// let string2 = ["1java2script3 5dave6lo4per"];
+// let counter1 = [];
+// let counter2 = [];
+
+// function str2(params) {
+//   for (let i = 0; i < params.length; i++) {
+//     if (typeof params[i] === "string") {
+//       counter1 = params[i];
+//       counter1++;
+//     } else {
+//       counter2 = params[i];
+//       counter2++;
+//     }
+//   }
+//   console.log(counter1);
+//   console.log(counter2);
+// }
+
+// str2(string2);
